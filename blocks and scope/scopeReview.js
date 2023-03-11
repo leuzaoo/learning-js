eda9b798b589a965d773742a37c69837eb19afb7
leuzaoo <leuzaoocontato@gmail.com>
2023-03-10 22:33:06 -0300

    scope review

---
+++ blocks and scope/scopeReview.js
@@ -0,0 +1,26 @@
+// Revisão: Escopo
+
+// Nesta lição, você aprendeu sobre escopo e como ele afeta a acessibilidade de diferentes variáveis.
+
+// Vamos revisar os seguintes termos:
+
+// O escopo refere-se a onde as variáveis ​​podem ser acessadas em todo o programa e é determinado por onde e como
+// elas são declaradas.
+
+// Blocos são instruções que existem entre chaves {}.
+
+// O escopo global refere-se ao contexto no qual as variáveis ​​são acessíveis a todas as partes do programa.
+
+// Variáveis ​​globais são variáveis ​​que existem dentro do escopo global.
+
+// O escopo do bloco refere-se ao contexto no qual as variáveis ​​são acessíveis apenas dentro do bloco em que são definidas.
+
+// Variáveis ​​locais são variáveis ​​que existem dentro do escopo do bloco.
+
+// Namespace global é o espaço em nosso código que contém informações com escopo global.
+
+// A poluição do escopo ocorre quando existem muitas variáveis ​​em um namespace ou nomes de variáveis ​​são reutilizados.
+
+// Conforme você continua sua jornada de codificação, lembre-se de usar as melhores práticas ao declarar suas variáveis!
+
+// Definir o escopo de suas variáveis ​​com precisão garantirá que seu código tenha uma lógica limpa, organizada e modular.
