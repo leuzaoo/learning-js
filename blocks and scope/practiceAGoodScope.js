4560f1e6ae62680173771d131c95f66c3a6c399f
leuzaoo <leuzaoocontato@gmail.com>
2023-03-10 22:22:55 -0300

    dont do that

---
+++ blocks and scope/practiceAGoodScope.js
@@ -0,0 +1,10 @@
+Pratique um bom escopo
+Dados os desafios com variáveis ​​globais e poluição de escopo, devemos seguir as práticas recomendadas para definir o escopo de nossas variáveis ​​o mais estritamente possível usando o escopo de bloco.
+
+O escopo rígido de suas variáveis ​​melhorará muito seu código de várias maneiras:
+
+Isso tornará seu código mais legível, pois os blocos organizarão seu código em seções discretas.
+Isso torna seu código mais compreensível, pois esclarece quais variáveis ​​estão associadas a diferentes partes do programa, em vez de ter que acompanhá-las linha após linha!
+É mais fácil manter seu código, já que seu código será modular.
+Isso economizará memória em seu código porque deixará de existir após o término da execução do bloco.
+Aqui está outro exemplo de como usar o escopo do bloco, conforme definido dentro de um ifbloco:
