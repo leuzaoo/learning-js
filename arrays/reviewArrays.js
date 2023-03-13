cf7fc0a6feff19d3ce182730c9f6747f9638848a
leuzaoo <leuzaoocontato@gmail.com>
2023-03-13 16:10:19 -0300

    review

---
+++ arrays/reviewArrays.js
@@ -0,0 +1,22 @@
+// Revisão de Arrays
+
+// Bom trabalho! Nesta lição, aprendemos estes conceitos sobre arrays:
+
+// Arrays são listas que armazenam dados em JavaScript.
+// Arrays são criados com colchetes [].
+// Cada item dentro de uma matriz está em uma posição numerada, ou índice, começando em 0.
+// Podemos acessar um item em um array usando seu índice, com sintaxe como: myArray[0].
+
+// Também podemos alterar um item em uma matriz usando seu índice, com sintaxe como myArray[0] = 'new string';****
+
+// Arrays possuem uma propriedade length, que permite que você veja quantos itens estão em um array.
+// Os arrays têm seus próprios métodos, incluindo .push() e .pop(), que adicionam e removem itens de um array, respectivamente.
+// Arrays têm muitos métodos que executam tarefas diferentes, como .slice() e .shift(), você pode encontrar a documentação no site Mozilla Developer Network .
+// Alguns métodos internos são mutantes, o que significa que o método mudará a matriz, enquanto outros não sofrem mutação. Você sempre pode verificar a documentação.
+// As variáveis ​​que contêm arrays podem ser declaradas com let ou const. Mesmo quando declarados com const, os arrays ainda são mutáveis. No entanto, uma variável declarada com const não pode ser reatribuída.
+
+// Arrays modificados dentro de uma função manterão essa mudança mesmo fora da função.****
+
+// Arrays podem ser aninhados dentro de outros arrays.
+// Para acessar elementos em matrizes aninhadas, encadeie índices usando a notação de colchetes.
+// Aprender a trabalhar e manipular arrays ajudará você a trabalhar com blocos de dados!
