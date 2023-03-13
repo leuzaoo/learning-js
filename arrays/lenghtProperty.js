1974d003d697439d161cbe0b9af6228e128304fe
leuzaoo <leuzaoocontato@gmail.com>
2023-03-13 15:17:44 -0300

    lenght

---
+++ arrays/lenghtProperty.js
@@ -0,0 +1,22 @@
+// A propriedade .length
+// Uma das propriedades internas de uma matriz é lengthe retorna o número de itens na matriz. Acessamos a
+// propriedade .length da mesma forma que fazemos com strings.
+
+// Confira o exemplo abaixo:
+
+const newYearsResolutions = ["Keep a journal", "Take a falconry class"];
+console.log(newYearsResolutions.length); // output: 2
+
+// No exemplo acima, registramos newYearsResolutions.length no console usando as seguintes etapas:
+
+// Usamos a notação de ponto, encadeando um ponto com o nome da propriedade para o array, para acessar a
+// propriedade length da array newYearsResolutions.
+// Em seguida, registramos o length de newYearsResolution no console.
+// Como newYearsResolution tem dois elementos, 2 sera registrado no console.
+
+// Quando queremos saber quantos elementos há em um array, podemos acessar a propriedade .length.
+
+// Encontre o length da objectives e registre-o no console.
+
+const objectives = ["Learn a new language", "Read 52 books", "Run a marathon"];
+console.log(objectives.length);
